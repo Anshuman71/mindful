bfee3c0c67a8172f9569ae6f1b17b738230b399f
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-08-07 17:34:03 +0530

    Use domain name instead of origin

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 const ONE_HOUR_IN_SECONDS = 3600
 const ONE_HOUR_IN_MILLISECONDS = ONE_HOUR_IN_SECONDS * 1000
-
+const REGEX_FOR_DOMAIN = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/
 const ADD_TO_GRACIOUS = 'add-to-mindful'
 
 chrome.runtime.onInstalled.addListener(
@@ -42,9 +42,9 @@ chrome.runtime.onInstalled.addListener(
 )
 
 chrome.webNavigation.onBeforeNavigate.addListener(({tabId, url}) => {
-    const urlObj = new URL(url)
+    const [_, domainName] = url.match(REGEX_FOR_DOMAIN)
     chrome.storage.local.get(['filters'], async function (filterResult) {
-        const filterUrl = Object.keys(filterResult.filters).filter(item => urlObj.origin.startsWith(item))
+        const filterUrl = Object.keys(filterResult.filters).filter(item => item.includes(domainName))
         if (filterUrl.length) {
             const filteredOrigin = filterUrl[0]
             const {lastVisitTime, hoursToWait} = filterResult.filters[filteredOrigin]
@@ -70,13 +70,13 @@ chrome.webNavigation.onBeforeNavigate.addListener(({tabId, url}) => {
 })
 
 chrome.contextMenus.onClicked.addListener(({pageUrl, menuItemId}) => {
-        const url = new URL(pageUrl)
+        const [url, domainName] = pageUrl.match(REGEX_FOR_DOMAIN)
         const hours = parseInt(menuItemId.split('-')[0])
         chrome.storage.local.get(['filters'], function (filterResult) {
             chrome.storage.local.set({
                 ['filters']: {
                     ...filterResult?.filters,
-                    [url.origin]: {hoursToWait: hours, lastVisitTime: Date.now()}
+                    [domainName]: {hoursToWait: hours, lastVisitTime: Date.now()}
                 }
             })
         })

c936c3ec2640687d5916ab61fdf5cfdc90a4bad2
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-08-09 13:51:08 +0530

    Update text

--- popup.js
+++ popup.js
@@ -4,8 +4,8 @@ const ONE_HOUR_IN_MILLISECONDS = ONE_HOUR_IN_SECONDS * 1000
 chrome.storage.local.get(['filters'], function (filterResult) {
     document.getElementById('origins').innerHTML = Object.keys(filterResult.filters).length ? Object.keys(filterResult.filters).map(item => {
         const {lastVisitTime, hoursToWait} = filterResult.filters[item]
-        const hoursFromLastVisit = Math.round((Date.now() - lastVisitTime) / ONE_HOUR_IN_MILLISECONDS)
-        const remainingHours = hoursToWait - hoursFromLastVisit
+        const hoursFromLastVisit = (Date.now() - lastVisitTime) / ONE_HOUR_IN_MILLISECONDS
+        const remainingHours = Math.round(hoursToWait - hoursFromLastVisit)
         return remainingHours > 0 ? `<li><span class="website">${item}</span> for <b>${remainingHours} hours</b></li>` : ``
     }).join('') : '<i>None so far :(</i>'
 })
